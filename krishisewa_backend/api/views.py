1f337eb096d20f92c57b0f5bb410e8c7758ac0dc
satwik2711 <satwik3.14159@gmail.com>
2023-04-23 20:54:04 +0530

    django backend for mandi prediction

---
+++ krishisewa_backend/api/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+import googlemaps
+from django.conf import settings
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def calculate_transport_cost(request):
+    origin = request.GET.get('origin')
+    quantity = request.GET.get('quantity')
+    destination_districts = settings.DISTRICTS
+
+
+    gmaps = googlemaps.Client(key=settings.GOOGLE_MAPS_API_KEY)
+
+    transport_cost = []
+
+
+    for destination in destination_districts:
+        # Skip the origin district
+        if origin == destination:
+            continue
+
+        result = gmaps.distance_matrix(origins=origin + ', India', destinations=destination + ', India', mode='driving')
+        distance = result['rows'][0]['elements'][0]['distance']['value']
+
+        cost = distance * 0.001 * 0.4 * quantity
+
+        # Add the transport cost to the list
+        transport_cost.append({
+            'destination': destination,
+            'cost': cost
+        })
+
+    # Return the transport cost as a JSON response
+    return Response({'transport_cost': transport_cost})
+
